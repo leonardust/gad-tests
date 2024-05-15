ae0c3762bfb4c1bdce08d5f9536042c9be63e3f6
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-05-15 20:03:36 +0200

    chore: added tags

--- README.md
+++ README.md
@@ -29,4 +29,16 @@ Run all tests:
 npx playwright test
 ```
 
+Run all test with tags:
+
+```
+npx playwright test --grep @GAD-R01-02
+```
+
+Run all test without tags:
+
+```
+npx playwright test --grep-invert @GAD-R01-02
+```
+
 For more usage cases look in `package.json` scripts section.
--- tests/webpage.spec.ts
+++ tests/webpage.spec.ts
@@ -4,7 +4,7 @@ import { HomePage } from '../src/pages/home.page';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify service main page', () => {
-  test('home page title', async ({ page }) => {
+  test('home page title @GAD-R01-01', async ({ page }) => {
     // Arrange
     const homePage = new HomePage(page);
 
@@ -16,7 +16,7 @@ test.describe('Verify service main page', () => {
     expect(title).toContain('GAD');
   });
 
-  test('articles page title', async ({ page }) => {
+  test('articles page title @GAD-R01-02', async ({ page }) => {
     // Arrange
     const articlesPage = new ArticlesPage(page);
 
@@ -28,7 +28,7 @@ test.describe('Verify service main page', () => {
     expect(title).toContain('Articles');
   });
 
-  test('comments page title', async ({ page }) => {
+  test('comments page title @GAD-R01-02', async ({ page }) => {
     // Arrange
     const commentsPage = new CommentsPage(page);
 
@@ -42,6 +42,6 @@ test.describe('Verify service main page', () => {
 
   test('home page title simple', async ({ page }) => {
     await page.goto('');
-    await expect(page).toHaveTitle('/GAD/');
+    await expect(page).toHaveTitle(/GAD/);
   });
 });
