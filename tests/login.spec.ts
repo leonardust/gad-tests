31c49e5681029a7e3a67b15663594ee22d0d2c33
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-05-16 23:08:40 +0200

    chore: env for email and password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,14 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD-R02-01', async ({ page }) => {
     // Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = testUser1.userPassword;
     const loginPage = new LoginPage(page);
-    const userEmail = 'Clementina_Bosco@test.test.dev';
-    const userPassword = '7kEND';
 
     // Act
     await loginPage.goTo();
