eeef79368e94c60a15f186817ee372ff95073125
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-07-15 13:09:58 +0200

    test: user can access article

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -24,21 +24,6 @@ test.describe('Verify articles', () => {
     await expect.soft(addArticleView.header).toBeVisible();
   });
 
-  test('create new article @GAD-R04-01', async ({ page }) => {
-    // Arrange
-    const articlePage = new ArticlePage(page);
-    const articleData = randomNewArticle();
-
-    // Act
-    await addArticleView.createArticle(articleData);
-
-    //Assert
-    await expect.soft(articlePage.articleTitle).toHaveText(articleData.title);
-    await expect
-      .soft(articlePage.articleBody)
-      .toHaveText(articleData.body, { useInnerText: true });
-  });
-
   test('reject creating article without title', async () => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
