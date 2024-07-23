b0a390c4c110402a3a24a16aee62c64a3d10b479
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-07-23 13:33:12 +0200

    chore: refactor to aaa formatting

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,12 +9,12 @@ test.describe('Verify login', () => {
     // Arrange
     const expectedWelcomeTitle = 'Welcome';
     const loginPage = new LoginPage(page);
+    const welcomePage = new WelcomePage(page);
 
     // Act
     await loginPage.goTo();
     await loginPage.login(testUser1);
 
-    const welcomePage = new WelcomePage(page);
     const title = await welcomePage.getTitle();
 
     //Assert
@@ -25,11 +25,12 @@ test.describe('Verify login', () => {
     // Arrange
     const expectedLoginTitle = 'Login';
     const expectedErrorMessage = 'Invalid username or password';
+    const loginPage = new LoginPage(page);
+
     const loginUserData: LoginUserModel = {
       userEmail: testUser1.userEmail,
       userPassword: 'incorrectPassword',
     };
-    const loginPage = new LoginPage(page);
 
     // Act
     await loginPage.goTo();
--- tests/smoke/menu.spec.ts
+++ tests/smoke/menu.spec.ts
@@ -10,11 +10,12 @@ test.describe('Verify main main buttons', () => {
     // Arrange
     const expectedCommentsTitle = 'Comments';
     const articlesPage = new ArticlesPage(page);
+    const commentsPage = new CommentsPage(page);
 
     // Act
     await articlesPage.goTo();
     await articlesPage.mainMenu.commentsButton.click();
-    const commentsPage = new CommentsPage(page);
+
     const title = await commentsPage.getTitle();
 
     // Assert
@@ -27,11 +28,12 @@ test.describe('Verify main main buttons', () => {
     // Arrange
     const expectedArticlesTitle = 'Articles';
     const commentsPage = new CommentsPage(page);
+    const articlesPage = new ArticlesPage(page);
 
     // Act
     await commentsPage.goTo();
     await commentsPage.mainMenu.articlesButton.click();
-    const articlesPage = new ArticlesPage(page);
+
     const title = await articlesPage.getTitle();
 
     // Assert
@@ -42,11 +44,12 @@ test.describe('Verify main main buttons', () => {
     // Arrange
     const expectedHomePageTitle = 'GAD';
     const articlesPage = new ArticlesPage(page);
+    const homePage = new HomePage(page);
 
     // Act
     await articlesPage.goTo();
     await articlesPage.mainMenu.homePage.click();
-    const homePage = new HomePage(page);
+
     const title = await homePage.getTitle();
 
     // Assert
