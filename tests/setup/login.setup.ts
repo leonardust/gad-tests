4d824b23b08b32045802833f94797bd7274be405
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-07-25 21:00:27 +0200

    chore: aliases for imports

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -1,10 +1,10 @@
-import { STORAGE_STATE } from '../../playwright.config';
+import { STORAGE_STATE } from '@_pw-config';
 import { LoginPage } from '@_src/pages/login.page';
 import { WelcomePage } from '@_src/pages/welcome.page';
 import { testUser1 } from '@_src/test-data/user.data';
 import { expect, test as setup } from '@playwright/test';
 
-setup('login with correct credentials @GAD-R02-01', async ({ page }) => {
+setup('login and save session', async ({ page }) => {
   // Arrange
   const expectedWelcomeTitle = 'Welcome';
   const loginPage = new LoginPage(page);
