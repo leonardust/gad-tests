38d65b734b4596ea4f6dd951d8195f86a58f22f5
Łukasz Komorowski <luk.komorowski@gmail.com>
2024-07-29 16:17:10 +0200

    chore: own fixtures in setup tests

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,12 +1,14 @@
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
 import { HomePage } from '@_src/pages/home.page';
+import { LoginPage } from '@_src/pages/login.page';
 import { test as baseTest } from '@playwright/test';
 
 interface Pages {
   articlesPage: ArticlesPage;
   commentsPage: CommentsPage;
   homePage: HomePage;
+  loginPage: LoginPage;
 }
 
 export const pageObjectTest = baseTest.extend<Pages>({
@@ -24,5 +26,10 @@ export const pageObjectTest = baseTest.extend<Pages>({
     const homePage = new HomePage(page);
     await homePage.goTo();
     await use(new HomePage(page));
+  },
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goTo();
+    await use(new LoginPage(page));
   },
 });
