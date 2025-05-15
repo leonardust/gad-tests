5de8fc1a3fb8a9077f0d159e7ff2d545baf7836a
Łukasz Komorowski <luk.komorowski@gmail.com>
2025-05-15 08:35:26 +0200

    chore: create ui package

--- src/ui/components/main-menu.component.ts
+++ src/ui/components/main-menu.component.ts
@@ -1,6 +1,7 @@
-import { ArticlesPage } from '@_src/pages/articles.page';
-import { CommentPage } from '@_src/pages/comment.page';
-import { HomePage } from '@_src/pages/home.page';
+import logger from '@_logger/index';
+import { ArticlesPage } from '@_src/ui/pages/articles.page';
+import { CommentPage } from '@_src/ui/pages/comment.page';
+import { HomePage } from '@_src/ui/pages/home.page';
 import { Page } from '@playwright/test';
 
 export class MainMenuComponent {
@@ -12,16 +13,19 @@ export class MainMenuComponent {
 
   async clicksCommentButton(): Promise<CommentPage> {
     await this.commentsButton.click();
+    logger.meth('Clicks comments button by: {el}', { el: this.commentsButton });
     return new CommentPage(this.page);
   }
 
   async clicksArticlesButton(): Promise<ArticlesPage> {
     await this.articlesButton.click();
+    logger.meth('Clicks articles button by: {el}', { el: this.articlesButton });
     return new ArticlesPage(this.page);
   }
 
   async clicksHomePageLink(): Promise<HomePage> {
     await this.homePageLink.click();
+    logger.meth('Clicks home page link by: {el}', { el: this.homePageLink });
     return new HomePage(this.page);
   }
 }
