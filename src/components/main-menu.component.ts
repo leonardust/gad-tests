9aed8dbcf873aadf1d5739a284009bd660a97231
Łukasz Komorowski <luk.komorowski@gmail.com>
2025-05-15 08:39:35 +0200

    Revert "chore: create ui package"
    This reverts commit 5de8fc1a3fb8a9077f0d159e7ff2d545baf7836a.

--- src/components/main-menu.component.ts
+++ src/components/main-menu.component.ts
@@ -1,7 +1,6 @@
-import logger from '@_logger/index';
-import { ArticlesPage } from '@_src/ui/pages/articles.page';
-import { CommentPage } from '@_src/ui/pages/comment.page';
-import { HomePage } from '@_src/ui/pages/home.page';
+import { ArticlesPage } from '@_src/pages/articles.page';
+import { CommentPage } from '@_src/pages/comment.page';
+import { HomePage } from '@_src/pages/home.page';
 import { Page } from '@playwright/test';
 
 export class MainMenuComponent {
@@ -13,19 +12,16 @@ export class MainMenuComponent {
 
   async clicksCommentButton(): Promise<CommentPage> {
     await this.commentsButton.click();
-    logger.meth('Clicks comments button by: {el}', { el: this.commentsButton });
     return new CommentPage(this.page);
   }
 
   async clicksArticlesButton(): Promise<ArticlesPage> {
     await this.articlesButton.click();
-    logger.meth('Clicks articles button by: {el}', { el: this.articlesButton });
     return new ArticlesPage(this.page);
   }
 
   async clicksHomePageLink(): Promise<HomePage> {
     await this.homePageLink.click();
-    logger.meth('Clicks home page link by: {el}', { el: this.homePageLink });
     return new HomePage(this.page);
   }
 }
--- src/fixtures/article.fixture.ts
+++ src/fixtures/article.fixture.ts
@@ -1,7 +1,7 @@
 import { prepareRandomNewArticle } from '@_src/factories/article.factory';
 import { pageObjectTest } from '@_src/fixtures/page-object.fixture';
 import { AddArticleModel } from '@_src/models/article.model';
-import { ArticlePage } from '@_src/ui/pages/article.page';
+import { ArticlePage } from '@_src/pages/article.page';
 
 interface ArticleCreationContext {
   articlePage: ArticlePage;
